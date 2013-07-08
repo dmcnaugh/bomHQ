b92b753af5fec60d43fb4472fa6db6413b55dd14
dmcnaughton <dmcnaughton@newington.nsw.edu.au>
2013-07-08 21:04:12 +1000

    Polling on bom.gov.au for image/png every 1 min for a new image every 6 min

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var express = require('express')
   , http = require('http')
   , path = require('path');
 
+var request = require('request');
+
 var app = express();
 
 // all environments
@@ -32,6 +34,43 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+function pad2(number) {
+    return (number < 10 ? '0' : '') + number
+}
+
+var REQ = function() {
+
+    var imgFile="http://www.bom.gov.au/radar/IDR712.T.";
+
+    var reqDate = new Date();
+    console.log(reqDate.toUTCString());
+    imgFile += reqDate.getUTCFullYear();
+    imgFile += pad2(reqDate.getUTCMonth()+1);
+    imgFile += pad2(reqDate.getUTCDate());
+    imgFile += pad2(reqDate.getUTCHours());
+    imgFile += pad2(Math.floor(reqDate.getUTCMinutes()/6)*6);
+    imgFile += '.png';
+    console.log(imgFile);
+
+    request({url: imgFile, method: 'HEAD'}, function (error, response, body) {
+        console.log(error);
+        console.log('SRV:'+response.statusCode);
+        console.log('SRV:'+response.headers.date)
+        console.log((body.length/1024).toFixed(2)+'Kb');
+        if (!error && response.statusCode == 200) {
+            request({url: imgFile, method: 'GET'}, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(response.headers['content-type']);
+                    console.log((body.length/1024).toFixed(2)+'Kb');
+                }
+            });
+        }
+    });
+};
+
+setInterval(REQ, 60000);
+
+//http.createServer(app).listen(app.get('port'), function(){
+//  console.log('Express server listening on port ' + app.get('port'));
+//});
+
