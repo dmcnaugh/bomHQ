aed70b86b20df988e1357d7f4afe0b6f86d1a0f7
dmcnaughton <dmcnaughton@newington.nsw.edu.au>
2013-08-03 18:29:07 +1000

    pull basic stats for CANT, SYD_OB, SYD_OLYP and added ability to query job queue via web /stats

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/img', pull.show);
+app.get('/stats', pull.jobStats);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- pull.js
+++ pull.js
@@ -59,19 +59,19 @@ var REQ = function(BOM_ID) {
          */
         console.log(error);
         console.log('BOM:'+response.statusCode);
-        console.log('BOM:'+response.headers.date)
-        console.log((body.length/1024).toFixed(2)+'Kb');
+        console.log('BOM:'+response.headers.date);
+//        console.log((body.length/1024).toFixed(2)+'Kb');
         if (!error && response.statusCode == 200) {
             /*
              * encoding: null - forces the request() to return body as a buffer (not string)
              */
             request({url: doc.imgFile, method: 'GET', encoding: null}, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
-                    console.log(response.headers['content-type']);
+//                    console.log(response.headers['content-type']);
 //                    console.log((body.length/1024).toFixed(2)+'Kb');
-                    console.log(body.length);
-                    console.log(typeof body);
-                    console.log(Buffer.isBuffer(body));
+//                    console.log(body.length);
+//                    console.log(typeof body);
+//                    console.log(Buffer.isBuffer(body));
 
                     doc.header = response.headers;
                     doc.image = body;
@@ -90,7 +90,64 @@ var REQ = function(BOM_ID) {
     });
 };
 
-//REQ();
+var STATS = function() {
+
+    var source = "http://www.bom.gov.au/nsw/observations/sydney.shtml";
+    var doc = {};
+
+    doc.reqDate = new Date();
+
+    var stations = [ 'canterbury' , 'sydney-observatory-hill', 'sydney-olympic-park'];
+
+    var temp = "<td headers=\"obs-temp obs-station-%STN%\"> *([0-9]*\\.[0-9]+) *</td>";
+    var rain = "<td headers=\"obs-rainsince9am obs-station-%STN%\"> *([0-9]*\\.[0-9]+) *</td>";
+    var hum = "<td headers=\"obs-relhum obs-station-%STN%\"> *([0-9]+) *</td>";
+
+    var tempRE = new RegExp(temp.replace('%STN%', stations[0]));
+    var rainRE = new RegExp(temp.replace('%STN%', stations[0]));
+    var humRE = new RegExp(hum.replace('%STN%', stations[0]));
+
+    request({url: source, method: 'HEAD'}, function (error, response, body) {
+        /*
+         *TODO: need to cope with error conditions here
+         */
+        console.log(error);
+        console.log('STATS:'+response.statusCode);
+        console.log('STATS:'+response.headers.date);
+//        console.log('STATS:'+(body.length/1024).toFixed(2)+'Kb');
+        if (!error && response.statusCode == 200) {
+
+            request({url: source, method: 'GET'}, function (error, response, body) {
+
+                doc.content = body;
+
+                for(i=0; i < stations.length; i++) {
+
+                    doc[stations[i]] = {};
+
+                    doc[stations[i]].temp = parseFloat(RegExp(temp.replace('%STN%', stations[i])).exec(body)[1]);
+                    doc[stations[i]].rain = parseFloat(RegExp(temp.replace('%STN%', stations[i])).exec(body)[1]);
+                    doc[stations[i]].hum = parseFloat(RegExp(hum.replace('%STN%', stations[i])).exec(body)[1]);
+
+                }
+
+                db.collection('OBS_SYD', function(err, collection) {
+                    if (err) throw err;
+                    collection.insert( doc , function(err, result) {
+                        if (err) throw err;
+                    });
+                });
+
+            });
+
+        }
+
+    });
+
+};
+
+
+//STATS();
 //setInterval(REQ, 60000);
 
 var rule = new schedule.RecurrenceRule();
@@ -104,21 +161,14 @@ for(var i = 5; i < 60; i += 6) {
 
 var bomTargets = [ 'IDR712', 'IDR713', 'IDR714' ];
 
-console.log(rule);
+//console.log(rule);
 
 var job = [];
 
-job[0] = schedule.scheduleJob(rule, function() { REQ(bomTargets[0]); });
-job[1] = schedule.scheduleJob(rule, function() { REQ(bomTargets[1]); });
-job[2] = schedule.scheduleJob(rule, function() { REQ(bomTargets[2]); });
-
-for (var i = 0; i < bomTargets.length; i++) {
-
-    console.log('JOB:'+bomTargets[i]);
-    console.log(job[i]);
-//    rule.second += 20;
-
-};
+job[0] = schedule.scheduleJob(bomTargets[0], rule, function() { REQ(bomTargets[0]); });    rule.second += 15;
+job[1] = schedule.scheduleJob(bomTargets[1], rule, function() { REQ(bomTargets[1]); });    rule.second += 15;
+job[2] = schedule.scheduleJob(bomTargets[2], rule, function() { REQ(bomTargets[2]); });    rule.second += 15;
+job[3] = schedule.scheduleJob('STATS', rule, function() { STATS(); });
 
 
 exports.show = function(req, res) {
@@ -131,4 +181,14 @@ exports.show = function(req, res) {
             console.log(result.image.buffer.length)
         });
     });
+}
+
+exports.jobStats = function(req, res) {
+
+    for(var i = 0; i < job.length; i++) {
+        job[i].go = job[i].showNextRun();
+        job[i].len = job[i].showQueueLength();
+    }
+
+    res.render('jobs', { title: 'Job Stats', jobs: job });
 }
