b20bde79ce8f6bceb00a6620ced0dfd57bc457a7
dmcnaughton <dmcnaughton@newington.nsw.edu.au>
2013-08-31 06:41:08 +1000

    secured with passport authentication, added font-awsome, refactored express routes

--- app.js
+++ app.js
@@ -4,22 +4,53 @@
  */
 
 var express = require('express')
-  , http = require('http')
-  , path = require('path')
+    , http = require('http')
+    , path = require('path')
+    , passport = require('passport')
+    , GoogleStrategy = require('passport-google').Strategy
     , io = require('socket.io');
 
 /**
  * GLOBALS
  */
-debug = require('express/node_modules/debug')('bom');
-app = express();
+debug = require('express/node_modules/debug')('bom'); // TODO: should not have to be global - currently referenced in routes/pull.js - use DI of (debug) instead
+app = express(); // TODO: should not have to be global - currently referenced in routes/pull.js - use DI of (app) instead
 
 var httpSrv = http.createServer(app);
-ioSrv = io.listen(httpSrv);
-//ioSrv.set('log level', 1);
 
-var pull = require('./routes/pull');
+ioSrv = io.listen(httpSrv);
+if ('production' == app.get('env')) {
+    ioSrv.set('log level', 1);
+}
+
 var api = require('./routes/socketApi')(ioSrv);
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+        // TODO: must cope with changing hostname and port numbers here
+        returnURL: 'http://192.168.1.143:3000/auth/google/return',
+        realm: 'http://192.168.1.143:3000/'
+    },
+    function(identifier, profile, done) {
+        // asynchronous verification, for effect...
+        process.nextTick(function () {
+
+            // To keep the example simple, the user's Google profile is returned to
+            // represent the logged-in user.  In a typical application, you would want
+            // to associate the Google account with a user record in your database,
+            // and return that user instead.
+            profile.identifier = identifier;
+            return done(null, profile);
+        });
+    }
+));
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -31,9 +62,26 @@ if ('development' == app.get('env')) {
 }
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-//app.use(express.cookieParser('your secret here'));
-//app.use(express.session());
+app.use(express.cookieParser());
+app.use(express.session({ secret: 'animal farm'})); //TODO: use a better secret
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(app.router);
+var routes = require('./routes/routes')(app, passport); // TODO: is this correct DI for app & passport?
+
+/**
+ * The following static content is exposed when not authenticated
+ * TODO: maybe this is better done with a separation of paths e.g. public vs. protected
+ */
+app.use('/stylesheets', express.static(path.join(__dirname, 'public/stylesheets')));
+app.use('/lib/bootstrap', express.static(path.join(__dirname, 'public/lib/bootstrap')));
+app.use('/lib/font-awesome', express.static(path.join(__dirname, 'public/lib/font-awesome')));
+/**
+ * All remaining static content is only accessible once the user is authenticated
+ */
+app.use(routes.ensureAuthenticated);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
@@ -41,20 +89,6 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-/**
- * TODO: Routes should be moved into a separate routes module.
- */
-
-//    app.get('/', function(req, res) { res.render('layout') });
-
-    app.get('/img/:range/:stamp', pull.show);
-//    app.get('/Radar', pull.radar);
-//
-//    app.get('/Chart', pull.chart)
-//
-//    app.get('/Jobs', pull.jobs);
-
-httpSrv.listen(app.get('port'), function(){
+httpSrv.listen(app.get('port'), function() {
   debug('Express server listening on port ' + app.get('port'));
 });
-
---
+++ routes/routes.js
@@ -0,0 +1,60 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: dave
+ * Date: 29/08/13
+ * Time: 6:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+var pull = require('./pull'); //TODO: this should have a DI of app.
+
+exports = module.exports = function(app, passport) {
+
+    app.get('/', function(req, res) {
+        res.render('index', { user: req.user})
+    });
+
+    // GET /auth/google
+    //   Use passport.authenticate() as route middleware to authenticate the
+    //   request.  The first step in Google authentication will involve redirecting
+    //   the user to google.com.  After authenticating, Google will redirect the
+    //   user back to this application at /auth/google/return
+    app.get('/auth/google',
+        passport.authenticate('google', { failureRedirect: '/' }),
+        function(req, res) {
+            res.redirect('/');
+        }
+    );
+
+    app.get('/auth/google/return',
+        passport.authenticate('google', { failureRedirect: '/' }),
+        function(req, res) {
+            res.redirect('/');
+        }
+    );
+
+    app.get('/auth/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+    /**
+     * Protected routes, i.e. must be an authenticated user to access these routes, including static content
+     */
+
+    app.get('/img/:range/:stamp', ensureAuthenticated, pull.show);
+
+    app.get('/account', ensureAuthenticated, function(req, res){
+        res.render('account', { user: req.user });
+    });
+
+    return module.exports;
+
+};
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) { return next(); }
+    res.send(403);
+}
+
+exports.ensureAuthenticated = ensureAuthenticated;
